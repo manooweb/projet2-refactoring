fbc80aa6fd1b0e6e2f735e612b52fe2f08fa9a7b
Emmanuel Hesry <ehesry@gmail.com>
2026-06-23 19:42:02 +0200

    Solve all lint errors espacialy type errors

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -2,6 +2,7 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute, ParamMap, Router, RouterLink } from '@angular/router';
 import Chart from 'chart.js/auto';
+import { Olympic, Participation } from 'src/app/models/olympic.model';
 
 
 @Component({
@@ -13,11 +14,11 @@ import Chart from 'chart.js/auto';
 })
 export class CountryComponent implements OnInit {
   private olympicUrl = './assets/mock/olympic.json';
-  public lineChart!: Chart<"line", string[], number>;
-  public titlePage: string = '';
-  public totalEntries: any = 0;
-  public totalMedals: number = 0;
-  public totalAthletes: number = 0;
+  public lineChart!: Chart<"line", number[], number>;
+  public titlePage = '';
+  public totalEntries = 0;
+  public totalMedals = 0;
+  public totalAthletes = 0;
   public error!: string;
 
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {
@@ -26,18 +27,18 @@ export class CountryComponent implements OnInit {
   ngOnInit() {
     let countryName: string | null = null
     this.route.paramMap.subscribe((param: ParamMap) => countryName = param.get('countryName'));
-    this.http.get<any[]>(this.olympicUrl).pipe().subscribe(
+    this.http.get<Olympic[]>(this.olympicUrl).pipe().subscribe(
       (data) => {
         if (data && data.length > 0) {
-          const selectedCountry = data.find((i: any) => i.country === countryName);
-          this.titlePage = selectedCountry.country;
-          const participations = selectedCountry?.participations.map((i: any) => i);
+          const selectedCountry = data.find((i: Olympic) => i.country === countryName);
+          this.titlePage = selectedCountry?.country ?? '';
+          const participations = selectedCountry?.participations.map((i: Participation) => i);
           this.totalEntries = participations?.length ?? 0;
-          const years = selectedCountry?.participations.map((i: any) => i.year) ?? [];
-          const medals = selectedCountry?.participations.map((i: any) => i.medalsCount.toString()) ?? [];
-          this.totalMedals = medals.reduce((accumulator: any, item: any) => accumulator + parseInt(item), 0);
-          const nbAthletes = selectedCountry?.participations.map((i: any) => i.athleteCount.toString()) ?? []
-          this.totalAthletes = nbAthletes.reduce((accumulator: any, item: any) => accumulator + parseInt(item), 0);
+          const years = selectedCountry?.participations.map(i => i.year) ?? [];
+          const medals = selectedCountry?.participations.map(i => i.medalsCount) ?? [];
+          this.totalMedals = medals.reduce((accumulator, item) => accumulator + item, 0);
+          const nbAthletes = selectedCountry?.participations.map(i => i.athleteCount) ?? []
+          this.totalAthletes = nbAthletes.reduce((accumulator, item) => accumulator + item, 0);
           this.buildChart(years, medals);
         }
       },
@@ -47,7 +48,7 @@ export class CountryComponent implements OnInit {
     );
   }
 
-  buildChart(years: number[], medals: string[]) {
+  buildChart(years: number[], medals: number[]) {
     const lineChart = new Chart("countryChart", {
       type: 'line',
       data: {
