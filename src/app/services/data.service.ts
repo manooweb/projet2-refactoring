8c42957cf2bc7d1b0cdfc1b01a799265b1ed5077
Emmanuel Hesry <ehesry@gmail.com>
2026-06-26 19:01:34 +0200

    Prepare getAllOlympics method before moving httpclient call from Dashboard Component

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Olympic } from '../models/olympic.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   private readonly httpClient = inject(HttpClient);
+
+  getAllOlympics(): Observable<Olympic[]> {
+    return of([]);
+  }
+
 }
