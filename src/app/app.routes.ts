9af5e14ed22e858c779af8a4543adbb106c4e8f4
Emmanuel Hesry <emmanuel.hesry@nantesmetropole.fr>
2026-06-26 16:24:28 +0200

    Refactor folder tree and move components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { CountryComponent } from './pages/country/country.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CountryComponent } from './country-detail/country.component';
+import { HomeComponent } from './dashboard/home.component';
+import { NotFoundComponent } from './not-found-page/not-found.component';
 
 export const routes: Routes = [
   {
