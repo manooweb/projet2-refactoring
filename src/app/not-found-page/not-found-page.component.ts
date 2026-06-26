e1512d749d0646335c7ab71a57a4a3fa4f44a5c3
Emmanuel Hesry <emmanuel.hesry@nantesmetropole.fr>
2026-06-26 16:44:18 +0200

    Rename NotFoundComponent to NotFoundPageComponent

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -3,11 +3,11 @@ import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-not-found',
-    templateUrl: './not-found.component.html',
-    styleUrls: ['./not-found.component.scss'],
+    templateUrl: './not-found-page.component.html',
+    styleUrls: ['./not-found-page.component.scss'],
     standalone: true,
     imports: [RouterLink]
 })
-export class NotFoundComponent {
+export class NotFoundPageComponent {
 
 }
