7f17a356aaf724e9844bdf4c0ea2df53019a8462
Emmanuel Hesry <ehesry@gmail.com>
2026-06-27 20:02:25 +0200

    Also add HeaderComponent to NotFoundPageComponent without title page

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
     selector: 'app-not-found',
     templateUrl: './not-found-page.component.html',
     styleUrls: ['./not-found-page.component.scss'],
     standalone: true,
-    imports: [RouterLink]
+    imports: [
+      RouterLink,
+      HeaderComponent
+    ]
 })
-export class NotFoundPageComponent {
 
+export class NotFoundPageComponent {
 }
