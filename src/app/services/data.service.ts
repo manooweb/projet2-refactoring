f46cca05382de2635c709537077e2d7093dcc8db
Emmanuel Hesry <ehesry@gmail.com>
2026-06-27 13:25:26 +0200

    Replace subscribe() in DashboardComponent with Observables and AsyncPipe

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Olympic } from '../models/olympic.model';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
